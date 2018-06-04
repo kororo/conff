967479a66cf0a07fe63df910f61bf39786c4fe0e
Robertus Johansyah <robertus@johansyah.com>
2018-06-04 10:21:17 +1000

    Refactor to utils

--- conff/parser.py
+++ conff/parser.py
@@ -2,23 +2,10 @@ import os
 import collections
 import copy
 import simpleeval
-from munch import Munch
 from simpleeval import EvalWithCompoundTypes
-from collections import OrderedDict as odict
 from cryptography.fernet import Fernet
 
-
-class Munch2(Munch):
-    """
-    Provide easy way to access item in object by dot-notation.
-    Example:
-        obj = {'item': 'value'}
-        # old way
-        value = obj.get('item')
-        # with munch
-        value = obj.item
-    """
-    pass
+from conff.utils import Munch2, update_recursive, yaml_safe_load, filter_value, odict
 
 
 class Parser:
@@ -118,8 +105,10 @@ class Parser:
             # print("Message: {}".format(ex))
             self.errors.append(ex)
         except Exception as ex:
+            # TODO: feature T2
             print('Exception on expression: {}'.format(expr))
             raise
+        # TODO: feature T4: include this part of the classes so user could override
         v = filter_value(v)
         return v
 
@@ -288,57 +277,3 @@ class Parser:
         # remove this specific foreach loop info from names dict so we don't
         # break any subsequent foreach loops
         del self._names['loop']
-
-
-def update_recursive(d, u):
-    """
-    Update dictionary recursively. It traverse any object implements
-    "collections.Mapping", anything else, it overwrites the original value.
-    :param d: Original dictionary to be updated
-    :param u: Value dictionary
-    :return: Updated dictionary after merged
-    """
-    for k, v in u.items():
-        if isinstance(d, dict):
-            d2 = d.get(k, {})
-        else:
-            d2 = getattr(d, k, object())
-
-        if isinstance(v, dict):
-            r = update_recursive(d2, v)
-            u2 = r
-        else:
-            u2 = u[k]
-
-        if isinstance(d, dict):
-            d[k] = u2
-        else:
-            setattr(d, k, u2)
-    return d
-
-
-def yaml_safe_load(stream):
-    import yaml
-    from yaml.resolver import BaseResolver
-
-    def ordered_load(stream, loader_cls):
-        class OrderedLoader(loader_cls):
-            pass
-
-        def construct_mapping(loader, node):
-            loader.flatten_mapping(node)
-            return odict(loader.construct_pairs(node))
-
-        OrderedLoader.add_constructor(BaseResolver.DEFAULT_MAPPING_TAG, construct_mapping)
-        return yaml.load(stream, OrderedLoader)
-
-    return ordered_load(stream, yaml.SafeLoader)
-
-
-def filter_value(value):
-    if isinstance(value, str):
-        if value == '[empty]':
-            value = ''
-    if isinstance(value, str):
-        value = value.strip()
-    return value
